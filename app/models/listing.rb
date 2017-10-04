70002897587daceed79253805ce75e609602a897
Julio Budal <juli0w@hotmail.com>
2017-10-04 09:52:25 -0300

    fix

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,8 +2,8 @@ class Listing < ApplicationRecord
   serialize :items, Array
 
   def populate
-    Listing.create(name: "Oficina", [6199, 6375, 1261, 1509,1343,1510,3939,75,
+    Listing.create({ name: "Oficina", [6199, 6375, 1261, 1509,1343,1510,3939,75,
         1538,5793,1520,1523,5684,1264,1494,1,1495,
-        3126,5,6,3110,5085,6341])
+        3126,5,6,3110,5085,6341] })
   end
 end
