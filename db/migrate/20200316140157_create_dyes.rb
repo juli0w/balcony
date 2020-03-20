29213ccdf2164de5a1eb9c6c90b48ca8be69a1cd
Julio Budal <juli0w@hotmail.com>
2020-03-20 11:17:31 -0300

    fix migration

--- db/migrate/20200316140157_create_dyes.rb
+++ db/migrate/20200316140157_create_dyes.rb
@@ -1,15 +1,15 @@
 class CreateDyes < ActiveRecord::Migration[5.0]
   def change
-    create_table :dye_inks do |t|
-      t.string :quantity
-      t.references :dye, foreign_key: true
-      t.references :ink, foreign_key: true
-    end
-
     create_table :dyes do |t|
       t.string :name
       t.decimal :price, :precision => 8, :scale => 2
       t.references :item, foreign_key: true
+    end
+    
+    create_table :dye_inks do |t|
+      t.string :quantity
+      t.references :dye, foreign_key: true
+      t.references :ink, foreign_key: true
     end
   end
 end
