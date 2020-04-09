c830bca112e1af893b8d43f4c6343e06bd92746e
Julio Budal <juli0w@hotmail.com>
2020-04-09 11:51:22 -0300

    fix migration

--- db/migrate/20200409135906_add_index_to_items_code.rb
+++ db/migrate/20200409135906_add_index_to_items_code.rb
@@ -1,8 +1,8 @@
 class AddIndexToItemsCode < ActiveRecord::Migration[5.0]
   def change
     add_index :items, :code, unique: true
-    add_index :family, :code, unique: true
-    add_index :group, :code, unique: true
-    add_index :subgroup, :code, unique: true
+    add_index :families, :code, unique: true
+    add_index :groups, :code, unique: true
+    add_index :subgroups, :code, unique: true
   end
 end
