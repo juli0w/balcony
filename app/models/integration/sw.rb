e885eb31a4a70ddb277ea81e39b830b9e5ce296a
Julio Budal <juli0w@hotmail.com>
2020-09-11 09:29:17 -0300

    skip 400000

--- app/models/integration/sw.rb
+++ app/models/integration/sw.rb
@@ -11,32 +11,34 @@ class Integration::Sw < Integration
                     encoding: 'iso-8859-1:utf-8',
                     col_sep: ';',
                     row_sep: :auto) do |row|
-                        
-            line = row.to_hash
-            
-            dye_name     = line["CORANTE"]
-            dye          = Dye.where(name: dye_name).first_or_create
-
-            product_name = line["PRODUTO"]
-            product      = SwProduct.where(name: product_name).first_or_create
-
-            base_name    = line["BASE"]
-            base         = SwBase.where(name: base_name).first_or_create
-
-            capacity     = line["CAPACIDADE"]
-            recipient    = SwRecipient.where(name: line["EMBALAGEM"],
-                                             capacity: capacity).first_or_create
+            if (n > 400000)     
+                line = row.to_hash
                 
-                ink = Ink.where(collection: line["COLECAO"],
-                                code: line["COD_COR"],
-                                name: line["NOME_COR"],
-                                message: line["MSG_AVISO"],
-                                sw_product: product,
-                                sw_base: base,
-                                sw_recipient: recipient).first_or_create
+                dye_name     = line["CORANTE"]
+                dye          = Dye.where(name: dye_name).first_or_create
+
+                product_name = line["PRODUTO"]
+                product      = SwProduct.where(name: product_name).first_or_create
+
+                base_name    = line["BASE"]
+                base         = SwBase.where(name: base_name).first_or_create
+
+                capacity     = line["CAPACIDADE"]
+                recipient    = SwRecipient.where(name: line["EMBALAGEM"],
+                                                capacity: capacity).first_or_create
                     
-            quantity     = line["MLS"]
-            ink.dye_inks.where(dye: dye, quantity: quantity).first_or_create
+                    ink = Ink.where(collection: line["COLECAO"],
+                                    code: line["COD_COR"],
+                                    name: line["NOME_COR"],
+                                    message: line["MSG_AVISO"],
+                                    sw_product: product,
+                                    sw_base: base,
+                                    sw_recipient: recipient).first_or_create
+                        
+                quantity     = line["MLS"]
+                ink.dye_inks.where(dye: dye, quantity: quantity).first_or_create
+
+            end
 
             n+=1
             # break if n > 4000
