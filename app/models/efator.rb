ed96f59fd7383a33a412fefea96691fa8ff17d4f
Julio Budal <juli0w@hotmail.com>
2020-09-28 11:40:16 -0300

    erro

--- app/models/efator.rb
+++ app/models/efator.rb
@@ -116,7 +116,8 @@ private
       subgroup = Subgroup.where(code: i.xpath("subgrupo").text).first
 
       # update item values
-      item.name      = i.xpath("descricao").text.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '*')
+      # item.name      = i.xpath("descricao").text.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '*')
+      item.name      = i.xpath("descricao").text
       item.family_id = family.try(:id)
       item.group_id  = group.try(:id)
       item.subgroup_id = subgroup.try(:id)
