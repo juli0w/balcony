76b5424e7d32e380e3b0661ee499f5d0072f6922
Julio Budal <juli0w@hotmail.com>
2020-11-09 09:22:17 -0300

    fix

--- db/migrate/20201106134804_add_price_to_order_items.rb
+++ db/migrate/20201106134804_add_price_to_order_items.rb
@@ -1,6 +1,6 @@
 class AddPriceToOrderItems < ActiveRecord::Migration[5.0]
   def change
-    add_column :order_items, :price, :decimal, :precision => 10, :scale => 2
+    # add_column :order_items, :price, :decimal, :precision => 10, :scale => 2
 
     OrderItem.all.each do |oi|
       oi.update(price: oi.item.try(:price))
