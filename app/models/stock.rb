ff488fa272665ccd01ac13dce09471c71051182d
Julio Budal <juli0w@hotmail.com>
2021-03-05 08:43:05 -0300

    fix

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,7 +4,7 @@ class Stock < ApplicationRecord
   has_many :stock_locations
 
   def of_item item_id
-    StockItem.where(stock_id: self.id, item_id: item_id).sum{|s| s.quantity.to_f }
+    StockItem.where(stock_id: self.id, item_id: item_id).sum{|s| s.quantity.to_d }
   end
 
   def location_of item_id
